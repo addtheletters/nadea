a74c8220564bfc6c64bba0e8059cb71ec36dcddb
addtheletters <bzhang.2003@gmail.com>
2014-10-19 12:00:10 -0400

    room and a bad script

---
+++ Assets/NadeThrow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NadeThrow : MonoBehaviour {
+
+	public GameObject nade_pre;
+	public float throwStrength;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Camera cam = Camera.main;
+			GameObject new_nade = (GameObject)Instantiate(nade_pre, cam.transform.position, cam.transform.rotation);
+			new_nade.rigidbody.AddForce(cam.transform.forward*throwStrength, ForceMode.Impulse);
+		}
+	}
+}
