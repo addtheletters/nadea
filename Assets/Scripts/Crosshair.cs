ee83ca7a6ac6f39d2f4e620fcebba5da0b88c93a
addtheletters <bzhang.2003@gmail.com>
2014-10-19 21:43:27 -0400

    something about nades

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+	//bad kludge crosshair
+
+	void OnGUI(){
+		GUI.Box(new Rect(Screen.width/2,Screen.height/2, 10, 10), "");
+	}
+}
